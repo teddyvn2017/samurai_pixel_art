8786ad5f91dd95a8372a739a9cc3fb648a2001fc
teddyvn2017 <ware4u@gmail.com>
2025-10-13 17:29:35 +0700

    adding function dash for player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.InputSystem;
@@ -16,12 +17,18 @@ public class PlayerController : MonoBehaviour
     private bool isGrounded;
 
     private bool wasGrounded = false;
-    private bool isLanded;
 
     public GameObject dustPrefab;
     public Transform dustPosition;
-    
-    private bool isJumping = false;   
+
+    [Header("Dash Settings")]
+    public float dashSpeed = 15f;       // Tốc độ lướt
+    public float dashTime = 0.2f; // thời gian lướt
+    public float dashCooldown = 1f;  // Thời gian hồi chiêu
+
+    private bool isDashing = false;
+    private float nextDashTime = 0f; // Thời điểm lần lướt tiếp theo có thể xảy ra  
+
     void Start()
     {
         anim = GetComponent<Animator>();
@@ -35,6 +42,7 @@ public class PlayerController : MonoBehaviour
         Jump();
         Attack();
         Landed();
+        HandleDashInput();
     }
     
     void Move()
@@ -101,9 +109,41 @@ public class PlayerController : MonoBehaviour
         }
         wasGrounded = isGrounded;
     }
-    
+
     void CreatingLandDust()
     {
         Instantiate(dustPrefab, dustPosition.position, dustPrefab.transform.rotation);
     }
-}
+
+    void HandleDashInput()
+    {
+        if (Keyboard.current.leftShiftKey.wasPressedThisFrame && Time.time >= nextDashTime && !isDashing)
+        {
+            StartCoroutine(PerformDash());            
+        }
+    }
+    
+    IEnumerator PerformDash()
+    {
+        isDashing = true;
+
+        // Lấy hướng Dash dựa trên hướng nhân vật đang quay mặt (local scale X)
+        Vector2 dashDirection = new Vector2(transform.localScale.x, 0).normalized; 
+
+        // Vector3 dashDirection = transform.forward;
+        // Áp dụng lực Dash
+        //rb.linearVelocity = dashDirection * dashSpeed;
+        rb.AddForce(dashDirection * dashSpeed, ForceMode2D.Impulse);
+        Debug.Log("linearVelocity x" + rb.linearVelocity.x +" "+rb.linearVelocity.y);
+        anim.SetTrigger("Dashing");
+
+        // 2. Đợi hết thời gian Dash
+        yield return new WaitForSeconds(dashTime);                
+
+        isDashing = false;
+    
+        // 4. gán lại giá trị nextDashTime cho lần kế tiếp
+        nextDashTime = Time.time + dashCooldown; 
+
+    }
+}   
